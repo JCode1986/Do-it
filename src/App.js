dc2e634f4ab80305bc8bbcce78f26fb6d2392d38
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-09-16 21:10:18 -0700

    added component for todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Todo from './Todo';
 
 function App() {
   //todos start off with empty array in use state
@@ -20,14 +21,18 @@ function App() {
     <div className="App">
     <h1>hello world!</h1>
     <form>
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <Button type="submit" onClick={addTodo} variant="contained" color="primary">
-        Add Todo
+      <FormControl>
+        <InputLabel>Write a Todo</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)}/>
+      </FormControl>
+
+      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+            Add Todo
       </Button>
     </form>
     <ul>
       {todos.map(todo => (
-        <li>{todo}</li>
+        <Todo text={todo}/>
         ))}
     </ul>
     </div>
