3dc6a07dbd916fb20eded281ac0e2facd3e63717
JC <joseph.hangarter@yahoo.com>
2020-10-20 15:09:39 -0700

    test

--- src/features/home/Hero.js
+++ src/features/home/Hero.js
@@ -6,29 +6,29 @@ import './Home.css'
 
 function Hero() {
     var items = [
-        <div class="container">
+        <div className="container">
             <img className="items" src={require("./heroImages/school.png")} alt="1" />
-            <Typography class="top-left"><strong>School</strong> - Write a detailed description of what needs to be studied</Typography>
-            <Typography class="bottom-left"></Typography>
-            <Typography class="centered"></Typography>
+            <Typography className="top-left"><strong>School</strong> - Write a detailed description of what needs to be studied</Typography>
+            <Typography className="bottom-left"></Typography>
+            <Typography className="centered"></Typography>
         </div>,
-            <div class="container">
+            <div className="container">
             <img className="items" style={{transform: 'rotateY(180deg)'}} src={require("./heroImages/work.png")} alt="2" />
-            <Typography class="top-left"><strong>Work</strong> - Sort work by priority levels</Typography>
-            <Typography class="bottom-left"></Typography>
-            <Typography class="centered"></Typography>
+            <Typography className="top-left"><strong>Work</strong> - Sort work by priority levels</Typography>
+            <Typography className="bottom-left"></Typography>
+            <Typography className="centered"></Typography>
         </div>,
-        <div class="container">
+        <div className="container">
             <img className="items" src={require("./heroImages/chores.png")} alt="3" />
-            <Typography class="top-left"><strong>Chores</strong> - Remind yourself of the chores and errands</Typography>
-            <Typography class="bottom-left"></Typography>
-            <Typography class="centered"></Typography>
+            <Typography className="top-left"><strong>Chores</strong> - Remind yourself of the chores and errands</Typography>
+            <Typography className="bottom-left"></Typography>
+            <Typography className="centered"></Typography>
         </div>,
-        <div class="container">
+        <div className="container">
             <img className="items" src={require("./heroImages/workout.png")} alt="4" />
-            <Typography class="top-left"><strong>Fitness</strong> - Check archive section on what was done on a particular workout</Typography>
-            <Typography class="bottom-left"></Typography>
-            <Typography class="centered"></Typography>
+            <Typography className="top-left"><strong>Fitness</strong> - Check archive section on what was done on a particular workout</Typography>
+            <Typography className="bottom-left"></Typography>
+            <Typography className="centered"></Typography>
         </div>,
 
     ]
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-//import firebase from "firebase";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
@@ -13,19 +12,4 @@ const firebaseApp = firebase.initializeApp({
     measurementId: "G-29NN95YC7V"
 });
 
-// export const firebaseApp = firebase.initializeApp({
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// });
-
 export default firebaseApp;
-
-//export const db = firebaseApp.firestore();
-
-//export default db;
